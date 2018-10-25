6f2c84c0610b7be180936b7a0b08d5596426eaf4
sanjiv.sahayam <sanjiv.sahayam@rea-group.com>
2018-10-25 17:25:45 +1100

    add placeholder tests

---
+++ src/test/scala/com/reagroup/exercises/validated/ValidatedExercisesSpec.scala
@@ -0,0 +1,49 @@
+package com.reagroup.exercises.validated
+
+import org.scalatest.{Matchers, WordSpecLike}
+import ValidationExercises._
+
+final class Spec extends Matchers with WordSpecLike {
+
+  val allBad = Map[String, String]()
+  val goodInput = Map("firstName" -> "Vladimir", "lastName" -> "Putin", "password" -> "crimea14")
+  val passwordIsTooShort = goodInput + ("password" -> "crim3a")
+  val passwordNoNumbers = goodInput + ("password" -> "crimeaasd")
+  val passwordNoNumbersAndTooShort = goodInput + ("password" -> "crime")
+  val noFirstName = goodInput - "firstName"
+  val noLastName = goodInput - "lastName"
+  val emptyFirstName = goodInput + ("firstName" -> "")
+  val emptyLastName = goodInput + ("lastName" -> "")
+
+  "Good input" in {
+    validateInput(goodInput) should be (Person("Vladimir", "Putin", "crimea14"))
+  }
+
+  // "All Bad input" in {
+  //   validateInput(allBad) should beFailing(NonEmptyList(keyNotFound("firstName"), keyNotFound("lastName"), keyNotFound("password")))
+  // }
+
+  // "password too short" in {
+  //   validateInput(passwordIsTooShort) should beFailing(NonEmptyList(passwordTooShort))
+  // }
+
+  // "password too weak" in {
+  //   validateInput(passwordNoNumbers) should beFailing(NonEmptyList(passwordTooWeak))
+  // }
+  // "password too short and too weak" in {
+  //   validateInput(passwordNoNumbersAndTooShort) should beFailing(NonEmptyList(passwordTooShort, passwordTooWeak))
+  // }
+  // "no first name" in {
+  //   validateInput(noFirstName) should beFailing(NonEmptyList(keyNotFound("firstName")))
+  // }
+  // "no last name" in {
+  //   validateInput(noLastName) should beFailing(NonEmptyList(keyNotFound("lastName")))
+  // }
+  // "empty first name" in {
+  //   validateInput(emptyFirstName) should beFailing(NonEmptyList(nameIsEmpty("firstName")))
+  // }
+  // "empty last name" in {
+  //   validateInput(emptyLastName) should beFailing(NonEmptyList(nameIsEmpty("lastName")))
+  // }
+
+}
