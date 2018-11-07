d23d0b42429d33738a3a6eeed2a746bf4c0bae2d
Jack Low <jack.low@rea-group.com>
2018-11-07 11:46:46 +1100

    Remove andThen exercises, simplify exercises a little.

--- src/main/scala/com/reagroup/exercises/validated/ValidatedExercises.scala
+++ src/main/scala/com/reagroup/exercises/validated/ValidatedExercises.scala
@@ -6,65 +6,29 @@ import cats.implicits._
 
 object ValidationExercises {
 
-  def validateKey(key: String, input: Map[String, String]): ValidatedNel[ErrorCode, String] =
-    input.get(key).fold[ValidatedNel[ErrorCode, String]](keyNotFound(key).invalidNel)(x => x.validNel)
+  case class Person(firstName: String, lastName: String, password: String)
 
-  def nameValidation(name: String, label: String): ValidatedNel[ErrorCode, String] =
-    if (name.trim.isEmpty) nameIsEmpty(label).invalidNel else name.validNel
+  sealed trait ValidationError
 
-  def passwordStrengthValidation(password: String): ValidatedNel[ErrorCode, String] =
-    if (!password.exists(Character.isDigit)) passwordTooWeak.invalidNel else password.validNel
+  case object PasswordTooShort extends ValidationError
 
-  def passwordLengthValidation(password: String): ValidatedNel[ErrorCode, String] =
-    if (password.length < 8) passwordTooShort.invalidNel else password.validNel
+  case object PasswordTooWeak extends ValidationError
 
-  def validateInput(input: Map[String, String]): ValidatedNel[ErrorCode, Person] =
-    (validateKey("firstName", input).andThen(nameValidation(_, "firstName")),
-     validateKey("lastName", input).andThen(nameValidation(_, "lastName")),
-     validateKey("password", input).andThen { password =>
-      passwordLengthValidation(password) |+| passwordStrengthValidation(password)
-    }).mapN(Person.apply)
+  case class NameIsEmpty(label: String) extends ValidationError
+
+  def nameValidation(name: String, label: String): ValidatedNel[ValidationError, String] =
+    Validated.condNel(name.trim.nonEmpty, name, NameIsEmpty(label))
+
+  def passwordStrengthValidation(password: String): ValidatedNel[ValidationError, String] =
+    Validated.condNel(password.exists(Character.isDigit), password, PasswordTooWeak)
+
+  def passwordLengthValidation(password: String): ValidatedNel[ValidationError, String] =
+    Validated.condNel(password.length > 8, password, PasswordTooShort)
+
+  def passwordValidation(password: String): ValidatedNel[ValidationError, String] =
+    passwordStrengthValidation(password) |+| passwordLengthValidation(password)
+
+  def validatePerson(firstName: String, lastName: String, password: String): ValidatedNel[ValidationError, Person] =
+    (nameValidation(firstName, "firstName"), nameValidation(lastName, "lastName"), passwordValidation(password)).mapN(Person(_, _, _))
+
 }
-
-case class Person(firstName: String, lastName: String, password: String)
-
-sealed trait ErrorCode
-
-case object passwordTooShort extends ErrorCode
-
-case object passwordTooWeak extends ErrorCode
-
-case class keyNotFound(key: String) extends ErrorCode
-
-case class nameIsEmpty(key: String) extends ErrorCode
-
-
-/*
-Interesting Validator combinators
-scala> val a:ValidationNel[String,String]  = "hi".successNel
-a: scalaz.ValidationNel[String,String] = Success(hi)
-scala> val b:ValidationNel[String,String]  = "world".successNel
-b: scalaz.ValidationNel[String,String] = Success(world)
-scala> val c:ValidationNel[String,String]  = "error1".failNel
-c: scalaz.ValidationNel[String,String] = Failure(NonEmptyList(error1))
-scala> val d:ValidationNel[String,String]  = "error2".failNel
-d: scalaz.ValidationNel[String,String] = Failure(NonEmptyList(error2))
-scala> a <* b
-res0: scalaz.Validation[scalaz.NonEmptyList[String],String] = Success(hi)
-scala> a *> b
-res1: scalaz.Validation[scalaz.NonEmptyList[String],String] = Success(world)
-scala> c <* d
-res2: scalaz.Validation[scalaz.NonEmptyList[String],String] = Failure(NonEmptyList(error1, error2))
-scala> a <* d
-res3: scalaz.Validation[scalaz.NonEmptyList[String],String] = Failure(NonEmptyList(error2))
-scala> a flatMap (hi => b)
-res4: scalaz.Validation[scalaz.NonEmptyList[String],String] = Success(world)
-scala> a flatMap (s => if (s == "hi") "hey back".successNel else "fine, be that way!".failNel)
-res6: scalaz.Validation[scalaz.NonEmptyList[String],String] = Success(hey back)
-scala> d flatMap (s => if (s == "hi") "hey back".successNel else "fine, be that way!".failNel)
-res7: scalaz.Validation[scalaz.NonEmptyList[String],String] = Failure(NonEmptyList(error2))
-scala> b flatMap (s => if (s == "hi") "hey back".successNel else "fine, be that way!".failNel)
-res8: scalaz.Validation[scalaz.NonEmptyList[String],String] = Failure(NonEmptyList(fine, be that way!))
-scala> a map (hi => hi + " worldz")
-res5: scalaz.Validation[scalaz.NonEmptyList[String],String] = Success(hi worldz)
- */
--- src/test/scala/com/reagroup/exercises/validated/ValidatedExercisesSpec.scala
+++ src/test/scala/com/reagroup/exercises/validated/ValidatedExercisesSpec.scala
@@ -3,76 +3,38 @@ package com.reagroup.exercises.validated
 import org.scalatest.FunSpec
 import ValidationExercises._
 import cats.data.NonEmptyList
-import org.scalatest.Matchers._
 import cats.data.Validated._
 
-final class ValidatedExercisesSpec extends FunSpec {
-
-  val allBad = Map[String, String]()
-  val goodInput = Map("firstName" -> "Vladimir", "lastName" -> "Putin", "password" -> "crimea14")
-  val passwordIsTooShort = goodInput + ("password" -> "crim3a")
-  val passwordNoNumbers = goodInput + ("password" -> "crimeaasd")
-  val passwordNoNumbersAndTooShort = goodInput + ("password" -> "crime")
-  val noFirstName = goodInput - "firstName"
-  val noLastName = goodInput - "lastName"
-  val emptyFirstName = goodInput + ("firstName" -> "")
-  val emptyLastName = goodInput + ("lastName" -> "")
-
-  // "Good input" in {
-  //   validateInput(goodInput) should be (Person("Vladimir", "Putin", "crimea14"))
-  // }
-
-  describe("validateKey") {
-    it("should fail when a key is not found") {
-      validateKey("firstName", allBad) should be (Invalid(NonEmptyList.of(keyNotFound("firstName"))))
-      validateKey("lastName", allBad)  should be (Invalid(NonEmptyList.of(keyNotFound("lastName"))))
-      validateKey("password", allBad)  should be (Invalid(NonEmptyList.of(keyNotFound("password"))))
-    }
-  }
+class ValidatedExercisesSpec extends FunSpec {
 
   describe("passwordLengthValidation") {
-    it("should fail when the password too short") {
-      passwordLengthValidation("crim3a") should be (Invalid(NonEmptyList.of(passwordTooShort)))
+    it("should fail when the password is too short") {
+      assert(passwordLengthValidation("crim3a") == Invalid(NonEmptyList.of(PasswordTooShort)))
     }
   }
 
   describe("passwordStrengthValidation") {
-    it("should fail when password too weak") {
-      passwordStrengthValidation("crimeaasd") should be (Invalid(NonEmptyList.of(passwordTooWeak)))
+    it("should fail when password is too weak") {
+      assert(passwordStrengthValidation("crimeaasd") == Invalid(NonEmptyList.of(PasswordTooWeak)))
+    }
+  }
+
+  describe("passwordValidation") {
+    it("should fail when the password is too short and too weak") {
+      assert(passwordValidation("") == Invalid(NonEmptyList.of(PasswordTooWeak, PasswordTooShort)))
     }
   }
 
   describe("nameValidation") {
     it("should fail when given an empty name") {
-      nameValidation("", "someLabel") should be (Invalid(NonEmptyList.of(nameIsEmpty("someLabel"))))
+      assert(nameValidation("", "someLabel") == Invalid(NonEmptyList.of(NameIsEmpty("someLabel"))))
     }
   }
 
-  describe("validateInput") {
-    describe("should fail when") {
-      it("password too weak") {
-        validateInput(passwordNoNumbers) should be (Invalid(NonEmptyList.of(passwordTooWeak)))
-      }
-
-      it("password too short and too weak"){
-        validateInput(passwordNoNumbersAndTooShort) should be (Invalid(NonEmptyList.of(passwordTooShort, passwordTooWeak)))
-      }
-
-      it("no first name"){
-        validateInput(noFirstName) should be (Invalid(NonEmptyList.of(keyNotFound("firstName"))))
-      }
-
-      it("no last name"){
-        validateInput(noLastName) should be (Invalid(NonEmptyList.of(keyNotFound("lastName"))))
-      }
-
-      it("empty first name"){
-        validateInput(emptyFirstName) should be (Invalid(NonEmptyList.of(nameIsEmpty("firstName"))))
-      }
-
-      it("empty last name"){
-        validateInput(emptyLastName) should be (Invalid(NonEmptyList.of(nameIsEmpty("lastName"))))
-      }
+  describe("validatePerson") {
+    it("should fail when given empty name and no password") {
+      assert(validatePerson("", "", "") == Invalid(NonEmptyList.of(NameIsEmpty("firstName"), NameIsEmpty("lastName"), PasswordTooWeak, PasswordTooShort)))
     }
   }
+
 }
