84de55fb6ecd39895a5f61912f6fb53d212f4643
Felipe Flores <felipe.flores@rea-group.com>
2019-03-20 15:36:12 +1100

    Add simple spec for AppRoutes.
    - Also change the signature for the dependencies passed on to the
      AppRoutes class.
      IMHO Controller details should not leak to the AppRoutes level,
      hence encoding them as Request[IO] => IO[Response[IO]].

---
+++ src/test/scala/com/reagroup/appliedscala/AppRoutesSpec.scala
@@ -0,0 +1,42 @@
+package com.reagroup.appliedscala
+
+import cats.effect.IO
+import com.reagroup.appliedscala.Http4sSpecHelpers._
+import org.http4s.dsl.Http4sDsl
+import org.http4s.testing.Http4sMatchers
+import org.http4s.{Request, Status}
+import org.specs2.mutable.Specification
+import org.specs2.specification.core.Fragment
+
+class AppRoutesSpec extends Specification with Http4sDsl[IO] with Http4sMatchers {
+
+  private val testAppRoutes = new AppRoutes(
+    fetchAllMoviesHandler = Ok("great titles"),
+    saveMoviesHandler = _ => NotFound()
+  )
+
+  "AppRoutes" should {
+    val endpoints: List[(Request[IO], String)] = List(
+      request(path = "/movies", method = GET) -> "great titles",
+      request(path = "/movies", method = POST) -> "movies created",
+      request(path = "/movies/reviews", method = POST) -> "reviews created"
+    )
+
+    Fragment.foreach(endpoints) { endpoint =>
+      val (req, expectedResponse) = endpoint
+
+      s"for ${req.method} ${req.uri}" in {
+
+        "return OK" in {
+          testAppRoutes.openRoutes.orNotFound(req) must returnValue(haveStatus(Status.Ok))
+        }
+
+        "return expected response" in {
+          body(testAppRoutes.openRoutes.orNotFound(req)) must beEqualTo(expectedResponse)
+        }
+      }
+    }
+  }
+
+}
+
---
+++ src/test/scala/com/reagroup/appliedscala/Http4sSpecHelpers.scala
@@ -0,0 +1,20 @@
+package com.reagroup.appliedscala
+
+import cats.effect.IO
+import org.http4s.util.CaseInsensitiveString
+import org.http4s.{Method, Query, Request, Response, Uri}
+
+object Http4sSpecHelpers {
+  def header(response: IO[Response[IO]], headerName: String): Option[String] = {
+    response.unsafeRunSync().headers.get(CaseInsensitiveString(headerName)).map(_.value)
+  }
+
+  def body(response: IO[Response[IO]]): String = {
+    val bytes = response.unsafeRunSync().body.compile.toVector.unsafeRunSync()
+    new String(bytes.toArray, "utf-8")
+  }
+
+  def request(path: String, query: String = "", method: Method = Method.GET): Request[IO] = {
+    Request[IO](method = method, uri = Uri(path = path, query = Query.fromString(query)))
+  }
+}
