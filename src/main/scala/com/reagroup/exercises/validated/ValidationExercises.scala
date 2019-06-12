c764e2e9d50e2bd012011bc52e65c39dafeea140
Arathy Janardhanan <arathy.janardhanan@rea-group.com>
2019-06-12 14:07:52 +1000

    Validation Exersice

--- src/main/scala/com/reagroup/exercises/validated/ValidationExercises.scala
+++ src/main/scala/com/reagroup/exercises/validated/ValidationExercises.scala
@@ -1,7 +1,7 @@
 package com.reagroup.exercises.validated
 
-import cats.data.Validated
-import cats.data.ValidatedNel
+import cats.data.{NonEmptyList, Validated, ValidatedNel}
+import cats.data.Validated.{Invalid, Valid}
 import cats.implicits._
 
 /**
@@ -38,7 +38,20 @@ object ValidationExercises {
     * Hint: Use the `.invalidNel` and `.validNel` combinators
     */
   def nameValidation(name: String, label: String): ValidatedNel[ValidationError, String] =
-    ???
+//  Soln 1
+//    if(name.isEmpty)
+//      NameIsEmpty(label).invalidNel
+//    else
+//      name.validNel
+
+  //  Soln 2
+//    if(name.isEmpty)
+//      Invalid(NonEmptyList.of(NameIsEmpty(label)))
+//    else
+//      Valid(name)
+
+  //  Soln 3
+  Validated.condNel(name.nonEmpty, name, NameIsEmpty(label))
 
   /**
     * If the `password` does not contain a numeric character, return a `PasswordTooWeak`.
@@ -48,7 +61,13 @@ object ValidationExercises {
     * Hint: Use `password.exists(Character.isDigit)`
     */
   def passwordStrengthValidation(password: String): ValidatedNel[ValidationError, String] =
-    ???
+//    if (password.exists(Character.isDigit))
+//      password.validNel
+//    else
+//      PasswordTooWeak.invalidNel
+
+    Validated.condNel(password.exists(Character.isDigit), password, PasswordTooWeak)
+
 
   /**
     * If the `password` length is not greater than 8 characters, return `PasswordTooShort`.
@@ -56,20 +75,30 @@ object ValidationExercises {
     * Otherwise, return the `password`.
     */
   def passwordLengthValidation(password: String): ValidatedNel[ValidationError, String] =
-    ???
+//    if (password.length >= 8)
+//      password.validNel
+//    else
+//      PasswordTooShort.invalidNel
+
+    Validated.condNel(password.length >= 8, password, PasswordTooShort)
 
   /**
     * Compose `passwordStrengthValidation` and `passwordLengthValidation` using Applicative `productR`
     * to construct a larger `passwordValidation`.
     */
   def passwordValidation(password: String): ValidatedNel[ValidationError, String] =
-    ???
+    passwordStrengthValidation(password)
+      .productR(passwordLengthValidation(password))
 
   /**
     * Compose `nameValidation` and `passwordValidation` to construct a function to `validatePerson`.
     */
   def validatePerson(firstName: String, lastName: String, password: String): ValidatedNel[ValidationError, Person] =
-    ???
+    (
+      nameValidation(firstName, "firstName"),
+      nameValidation(lastName, "lastName"),
+      passwordValidation(password)
+    ).mapN((firstName, lastName, password) => Person(firstName, lastName, password))
 
 
   /**
@@ -80,6 +109,18 @@ object ValidationExercises {
   type LastName = String
   type Password = String
   def validatePeople(inputs: List[(FirstName, LastName, Password)]): ValidatedNel[ValidationError, List[Person]] =
-    ???
+//    inputs.map(person => validatePerson(person._1, person._2, person._3)).sequence
+//    inputs.traverse(person => validatePerson(person._1, person._2, person._3))
+    inputs.traverse {
+      case (firstName, lastName, password) => validatePerson(firstName, lastName, password)
+    }
 
+
+//  ways to fetch from tuple
+//  val t = (1, 2, 3)
+//  val (t1, t2, t3) = t
+//  t._1
+//  list.map{
+//    case (t1, t2, t3) =>
+//  }
 }
